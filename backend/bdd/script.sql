cc25ac962da4351d88c58207d267e42cee634ac7
Madwest72 <mamadou.abdiallo@outlook.fr>
2026-06-24 17:23:35 +0200

    first

---
+++ backend/bdd/script.sql
@@ -0,0 +1,27 @@
+create database playbook;
+
+use playbook;
+
+create table recette(
+    id int auto_increment primary key,
+    nom VARCHAR(255),
+    information text,
+    instrution text
+);
+
+create table ingredient(
+    id int  auto_increment primary key,
+    calorie float,
+    protein float,
+    lipide float,
+    glucide float
+);
+
+create table recette_ingredient(
+    quantite float,
+    recette_id int,
+    ingredient_id int,
+    foreign key (recette_id) references recette(id) ON DELETE CASCADE,
+    foreign key (ingredient_id) references ingredient(id) ON DELETE CASCADE,
+    PRIMARY KEY (recette_id, ingredient_id)
+);
